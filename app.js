25cd56c9317868e0ec4a0ebde00f4e45fcdc5da1
shubham078 <stiwari@cognitus.com>
2024-07-27 13:17:11 +0530

    added the serverless configuraration

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose')
 const cors = require('cors');
-const morgan = require('morgan');
 
-app.use(morgan());
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(cors('Access-Control-Allow-Origin', '*'));
 
-connection_uri = "mongodb://localhost:27017/";
+connection_uri = "mongodb+srv://quasarbytes:wHKYdAj2Z9Y5GQFf@cluster0.apczzwy.mongodb.net/";
 mongoose.Promise = global.Promise;
-system_db = mongoose.connect(connection_uri.concat("quasarBytes"),{ useFindAndModify: false }, {
+system_db = mongoose.connect(connection_uri, { useFindAndModify: false }, {
     native_parser : true
 }, function(err){
     if (err) throw err;
@@ -28,7 +26,6 @@ const QuasarContact = require('./api/routes/QuasarContact')
 app.use('/connect',QuasarContact)
 
 
-app.use(express.static(__dirname + '/assets/'));
 app.use((req,res,next) => {
     const error = new Error('Not found');
     error.status = 404;
--- handler.js
+++ handler.js
@@ -1,18 +1,45 @@
-var express = require('express');
-var http = require('http')
-var https = require('https')
-const app = require('./app');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors');
 
-const options = {
-    // /home/ubuntu/QuasarBytesBackend
-    // key:fs.readFileSync('/home/ubuntu/QuasarBytesBackend/key.pem'),
-    // cert:fs.readFileSync('/home/ubuntu/QuasarBytesBackend/cert.pem')
-    key:fs.readFileSync('/home/ubuntu/ssl/private.key'),
-    cert:fs.readFileSync('/home/ubuntu/ssl/certificate.crt'),
-    bundle:fs.readFileSync('/home/ubuntu/ssl/ca_bundle.crt'),
+app.use(express.urlencoded({
+    extended: true
+}));
 
-}
-// var server = http.Server(app);
-const server = https.createServer(options, app);
-server.listen(4001, () => console.log('listening on *:4001'));
+app.use(cors('Access-Control-Allow-Origin', '*'));
+
+connection_uri = "mongodb+srv://quasarbytes:wHKYdAj2Z9Y5GQFf@cluster0.apczzwy.mongodb.net/";
+mongoose.Promise = global.Promise;
+system_db = mongoose.connect(connection_uri, { useFindAndModify: false }, {
+    native_parser : true
+}, function(err){
+    if (err) throw err;
+});
+mongoose.set('useUnifiedTopology', true);
+
+
+const QuasarContact = require('./api/routes/QuasarContact')
+
+
+app.use('/connect',QuasarContact)
+
+
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 500);
+    res.json({
+            responseCode: 404,
+            responseMessage: error.message
+        
+    })
+})
+
+
+module.exports = app;
